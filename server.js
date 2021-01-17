eb7a6c790cba739e96a01c18e8534d655b3cf4d5
Richard Hwang <hackcomj@hotmail.com>
2021-01-17 12:11:47 -0800

    added dotfiles allow

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app
   .prepare()
   .then(() => {
     const server = express();
-    server.use('/.well-known/acme-challenge', express.static('/home/site/wwwroot/.well-known/acme-challenge'));
+    server.use('/.well-known/acme-challenge', express.static('/home/site/wwwroot/.well-known/acme-challenge', {dotfiles: 'allow'}));
     server.get('*', (req, res) => handle(req, res));
     server.listen(port, err => {
       if(err) throw err;
